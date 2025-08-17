ef97c7aa608f5addf6db25669d4198a76bce08d0
coder6970 <johnnypapa6970@gmail.com>
2025-08-17 23:06:30 +0530

    started city repo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {port} = require("./config/serverconfig")
+const CityRepository = require("./repository/city-repo.js");
 const setupAndStartServer = async () => {
     //create express object
     const app = express();
@@ -8,6 +9,9 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended : true}));
     app.listen(port,()=>{
         console.log(`server started at ${port}`)
+        const cityRepo = new CityRepository();
+        
+        cityRepo.createCity({name : "Delhi"});
     })
 }
 setupAndStartServer();
--- src/models/city.js
+++ src/models/city.js
@@ -14,7 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   City.init({
-    name: DataTypes.STRING
+    name: {type : DataTypes.STRING,
+    allowNull: false,
+    unique: true},
   }, {
     sequelize,
     modelName: 'City',
