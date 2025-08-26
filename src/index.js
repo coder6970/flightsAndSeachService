413d192d85acbf42457a74c690a2d2746db3be2b
coder6970 <johnnypapa6970@gmail.com>
2025-08-27 00:55:38 +0530

    seeders added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const {port} = require("./config/serverconfig")
-const CityRepository = require("./repository/city-repo.js");
-const apiRoutes = require('./routes/index.js')
+const apiRoutes = require('./routes/index.js');
+const { sequelize } = require("./models/index.js");
+const {City,Airport} = require ('./models/index.js')
 
 const setupAndStartServer = async () => {
     //create express object
@@ -11,8 +12,16 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.urlencoded({extended : true}));
     app.use('/api', apiRoutes);
 
-    app.listen(port,()=>{
-        console.log(`server started at ${port}`)
+    app.listen(port,async()=>{
+        console.log(`server started at ${port}`);
+        //db.sequelize.sync({alter : true});
+        // const city = await City.findOne({
+        //     where : {
+        //         id: 1
+        //     }
+        // });
+        // const airports = await city.getAirports();
+        // console.log(city,airports)
     })
 }
 setupAndStartServer();
---
+++ src/seeders/20250826135435-add_airport.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+        await queryInterface.bulkInsert('Airports',[
+          {
+            name : "Indira Gandhi Internation Airtport",
+            city_id : 1,
+            createdAt : new Date(),
+            updatedAt : new Date()
+          },
+          {
+            name : "Safdarjung Airtport",
+            city_id : 1,
+            createdAt : new Date(),
+            updatedAt : new Date()
+          },
+          {
+            name : "Raja Bhoj Airtport",
+            city_id : 8,
+            createdAt : new Date(),
+            updatedAt : new Date()
+          }
+        ],{})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
