b984e581666630d8636a72b5123b9d9cb8282c40
coder6970 <johnnypapa6970@gmail.com>
2025-08-26 19:23:11 +0530

    association added

---
+++ src/migrations/20250826100845-create-airport.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Airports', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      city_id: {
+        type: Sequelize.INTEGER,
+        onDelete : "CASCADE",
+        references : {
+          model : 'Cities',
+          key : 'id',
+          as : 'city_id'
+        },
+        allowNull : false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Airports');
+  }
+};
